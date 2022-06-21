498dcd43c4bb0368ba3db19928f5b91af12a7a15
Jackie Weng <jackie@jemeraldsoft.co.nz>
2022-06-21 21:19:44 +1200

    Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
-const App:React.FC = () => (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  )
+const theme = createTheme();
+
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <main>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Home
+          </Typography>
+        </Container>
+      </Box>
+    </main>
+  </ThemeProvider>
+);
 
 export default App;
