49b3875e785c57c6cdd6d32b5325a2d3234c2418
Jackie Weng <jackie@jemeraldsoft.co.nz>
2022-06-21 21:06:07 +1200

    Arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+const App:React.FC = () => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
       </header>
     </div>
-  );
-}
+  )
 
 export default App;
