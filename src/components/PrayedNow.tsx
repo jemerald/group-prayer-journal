cd121ed2da3ee17c063989d44ce5df888901e876
Jackie Weng <jackie@jemeraldsoft.co.nz>
2023-03-13 22:39:15 +1300

    fix: added optimistic update to prevent pray now button from being enabled

--- src/components/PrayedNow.tsx
+++ src/components/PrayedNow.tsx
@@ -1,5 +1,5 @@
 import Button from "@mui/material/Button";
-import type { PrayerItem } from "@prisma/client";
+import type { PrayerItem, Timeline } from "@prisma/client";
 import isSameDay from "date-fns/isSameDay";
 import React from "react";
 import { trpc } from "../utils/trpc";
@@ -13,14 +13,42 @@ const PrayedNow: React.FC<{
 
   const utils = trpc.useContext();
   const mutation = trpc.timeline.prayedNow.useMutation({
-    onSuccess(added) {
+    onMutate(variable) {
+      // perform optimistic update
+      utils.timeline.allByItemId.cancel({
+        itemId: item.id,
+      });
+      const tempTimelineItem: Timeline = {
+        id: "dummy",
+        itemId: variable.itemId,
+        targetId: item.targetId,
+        type: "PRAYED",
+        date: new Date(),
+        note: null,
+      };
       utils.timeline.allByItemId.setData(
         {
           itemId: item.id,
         },
-        (currentTarget) => [added, ...(currentTarget || [])]
+        (oldData) => [tempTimelineItem, ...(oldData ?? [])]
       );
+      utils.timeline.lastPrayedForItem.setData(
+        { itemId: item.id },
+        () => tempTimelineItem
+      );
+      utils.timeline.lastPrayedForTarget.setData(
+        {
+          targetId: item.targetId,
+        },
+        () => tempTimelineItem
+      );
+    },
+    onSuccess() {
       utils.timeline.allByItemId.invalidate({ itemId: item.id });
+      utils.timeline.lastPrayedForItem.invalidate({ itemId: item.id });
+      utils.timeline.lastPrayedForTarget.invalidate({
+        targetId: item.targetId,
+      });
     },
   });
   if (lastPrayed.isLoading || lastPrayed.data === undefined) {
