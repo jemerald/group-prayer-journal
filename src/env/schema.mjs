365a35d54f8e8e3b5ff6e5608121496d10ac79cc
Jackie Weng <jweng@linz.govt.nz>
2024-04-07 21:11:37 +1200

    feat: allow test user login for non-production environments

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -22,6 +22,7 @@ export const serverSchema = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   UNSPLASH_ACCESS_KEY: z.string(),
+  TEST_USER_SECRET: z.string().optional(),
 });
 
 /**
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,61 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import type { Provider } from "next-auth/providers";
+
+const TestUserId = "test-user-1";
+
+const testCredentialProviders: Provider[] = env.TEST_USER_SECRET
+  ? [
+      CredentialsProvider({
+        name: "Test user secret",
+        credentials: {
+          secret: { label: "Test user secret", type: "password" },
+        },
+        async authorize(credentials) {
+          if (credentials?.secret === env.TEST_USER_SECRET) {
+            let user = await prisma.user.findFirst({
+              where: {
+                id: TestUserId,
+              },
+            });
+            if (user == null) {
+              user = {
+                id: TestUserId,
+                name: "Test User",
+                email: "test@local.host",
+                emailVerified: null,
+                image: null,
+              };
+              await prisma.user.create({
+                data: user,
+              });
+            }
+            return user;
+          }
+          return null;
+        },
+      }),
+    ]
+  : [];
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
+    session({ session, token, user }) {
       if (session.user) {
-        session.user.id = user.id;
+        if (user) {
+          // this works for external IDP login
+          session.user.id = user.id;
+        } else if (token.sub) {
+          // this is needed to make test user work
+          session.user.id = token.sub;
+        }
       }
       return session;
     },
@@ -19,6 +63,7 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
+    ...testCredentialProviders,
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
@@ -26,12 +71,15 @@ export const authOptions: NextAuthOptions = {
         params: {
           prompt: "consent",
           access_type: "offline",
-          response_type: "code"
-        }
-      }
-    })
+          response_type: "code",
+        },
+      },
+    }),
     // ...add more providers here
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default NextAuth(authOptions);
