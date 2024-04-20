9698868c0b768c6c3ea26a7c6cb0e39bba9d88a6
Jackie Weng <jweng@linz.govt.nz>
2024-04-20 21:01:08 +1200

    fix: unsplash API call

--- src/server/trpc/utils/randomPhoto.ts
+++ src/server/trpc/utils/randomPhoto.ts
@@ -1,12 +1,14 @@
 import { env } from "../../../env/server.mjs";
 
-export async function getRandomPhoto(
-  topic = "nature"
-): Promise<{ url: string; color: string; blurHash: string } | null> {
+export async function getRandomPhoto(): Promise<{
+  url: string;
+  color: string;
+  blurHash: string;
+} | null> {
   const accessKey = env.UNSPLASH_ACCESS_KEY;
   if (accessKey) {
     const resp = await fetch(
-      `https://api.unsplash.com/photos/random?topic=${topic}`,
+      `https://api.unsplash.com/photos/random?topics=6sMVjTLSkeQ`, // with nature topic
       {
         headers: {
           Authorization: `Client-ID ${accessKey}`,
