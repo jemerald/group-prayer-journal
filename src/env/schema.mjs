c71b9ca6df35c6d1d4f44a824f040fe2aa084374
jackie-linz <120340761+jackie-linz@users.noreply.github.com>
2024-04-23 20:44:21 +1200

    chore: make unsplash optional (#182)

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -21,7 +21,7 @@ export const serverSchema = z.object({
   ),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
-  UNSPLASH_ACCESS_KEY: z.string(),
+  UNSPLASH_ACCESS_KEY: z.string().optional(),
   TEST_USER_SECRET: z.string().optional(),
 });
 
