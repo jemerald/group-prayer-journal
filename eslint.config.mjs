acfc3a29efb5d649d42fab913a046fc7fe86776f
Jackie Weng <jweng@linz.govt.nz>
2025-12-10 23:08:59 +1300

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -14,23 +16,3 @@ const eslintConfig = defineConfig([
 ]);
 
 export default eslintConfig;
-// import eslint from "@eslint/js";
-// import { defineConfig } from "eslint/config";
-// import tseslint from "typescript-eslint";
-
-// export default defineConfig(
-//   eslint.configs.recommended,
-//   tseslint.configs.recommended
-// );
-
-// {
-//   "parser": "@typescript-eslint/parser",
-//   "parserOptions": {
-//     "project": "./tsconfig.json"
-//   },
-//   "plugins": ["@typescript-eslint"],
-//   "extends": ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-//   "rules": {
-//     "@typescript-eslint/consistent-type-imports": "warn"
-//   }
-// }
