53d2b8477c6f412eede17dae7d9189a69b9e97f1
Jackie Weng <jweng@linz.govt.nz>
2026-04-14 06:26:55 +1200

    fix: setup connection

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -7,7 +7,18 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const adapter = new PrismaMariaDb({});
+const connectionUrl = new URL(env.DATABASE_URL);
+// console.log("Connecting to database with URL:", env.DATABASE_URL);
+const options = {
+  host: connectionUrl.hostname,
+  port: Number(connectionUrl.port) || 3306,
+  user: connectionUrl.username,
+  password: connectionUrl.password,
+  database: connectionUrl.pathname.slice(1),
+  ssl: true,
+};
+// console.log("Connecting to database with options:", options);
+const adapter = new PrismaMariaDb(options);
 export const prisma =
   global.prisma ||
   new PrismaClient({
